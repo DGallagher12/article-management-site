c239be17d3100229137a511604914b5c3652a638
DGallagher12 <x22163671@student.ncirl.ie>
2023-05-08 17:29:27 +0100

    updates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,10 @@
 class Article < ApplicationRecord
+
+  def get_article_status
+    if(self.published)
+      return "Completed"
+    else
+     return "Not ready"
+    wns
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save article" do
+    article=Article.new
+    assert article.save
+  end
+
+  test "find article" do
+    article = Article.new()
+     article.title = "How to cook fish"
+     article.body = "Put it in the oven"
+     article.published = false
+     article.save
+
+     expected_article = Article.find(article.id)
+     assert_equal(expected_article.title, article.title)
+     assert_equal(expected_article.body, article.body)
+     assert_equal(expected_article.published, article.published)
+  end
+
+  test "delete article" do
+     article=Article.new
+     article.save
+     assert_difference("Article.count", -1) do
+       article.destroy
+     end
+
+  end
+
+  test "update article" do
+     one = articles(:one)
+     one.update({
+      title: "How to paint a fence"
+     })
+     assert_equal("How to paint a fence", one.title)
+  end
+
+  test "should be marked as avalable when published" do
+    one = articles(:one)
+    assert_equal(one.get_article_status(), "Completed")
+  end
+
+  test "should be not ready when not published" do
+    two = articles(:two)
+    assert_equal(two.get_article_status(), "Not ready")
+  end
 end
